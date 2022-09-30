8c597e7d3efeff57db64a2a98831b1b7149509c4
Gabrielfortuna <gabrielsi8@hotmail.com>
2022-09-30 09:03:44 -0300

    subindo

--- front/js/anima.js
+++ front/js/anima.js
@@ -1,10 +1,10 @@
 let Anima = {
-
-    ArraySVG: [{
-        nome: 'triangulo',
-        posX: -6,
-        posY: 6,
-        svg: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="197" height="198" viewBox="0 0 197 198">
+  ArraySVG: [
+    {
+      nome: "triangulo",
+      posX: -6,
+      posY: 6,
+      svg: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="197" height="198" viewBox="0 0 197 198">
             <defs>
             <clipPath id="clip-triangulo">
                 <rect width="197" height="198"/>
@@ -20,11 +20,13 @@ let Anima = {
             </g>
             </g>
         </svg>
-        `}, {
-            nome: 'piramide',
-            posX: -10,
-            posY: 70,
-            svg: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="237.57" height="260.2" viewBox="0 0 237.57 260.2">
+        `,
+    },
+    {
+      nome: "piramide",
+      posX: -10,
+      posY: 70,
+      svg: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="237.57" height="260.2" viewBox="0 0 237.57 260.2">
         <defs>
           <clipPath id="clip-piramide">
             <rect width="237.57" height="260.2"/>
@@ -41,30 +43,74 @@ let Anima = {
           </g>
         </g>
       </svg>
-      `
-    }],
+      `,
+    },
+    {
+      nome: "planeta",
+      posX: 61,
+      posY: 25,
+      svg: `
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="128" height="73" viewBox="0 0 128 73">
+  <defs>
+    <clipPath id="clip-planeta">
+      <rect width="128" height="73"/>
+    </clipPath>
+  </defs>
+  <g id="planeta" clip-path="url(#clip-planeta)">
+    <g id="Grupo_339" data-name="Grupo 339" transform="translate(-87.893 -31.825)">
+      <ellipse id="Elipse_17" data-name="Elipse 17" cx="35.296" cy="35.5" rx="35.296" ry="35.5" transform="translate(116.598 32.825)" fill="#ff4081"/>
+      <path id="Elipse_17_-_Contorno" data-name="Elipse 17 - Contorno" d="M35.3-1A36.084,36.084,0,0,1,60.963,9.693a36.6,36.6,0,0,1,0,51.615A36.315,36.315,0,0,1-1,35.5,36.512,36.512,0,0,1,21.166,1.87,35.9,35.9,0,0,1,35.3-1Zm0,71A34.5,34.5,0,1,0,1,35.5,34.437,34.437,0,0,0,35.3,70Z" transform="translate(116.598 32.825)" fill="#2a2929"/>
+      <path id="Caminho_2561" data-name="Caminho 2561" d="M185.19,56.152l-.014.008a35.71,35.71,0,0,1,1.865,7.39h0c9.916,1.4,16.106,3.384,16.106,5.584,0,4.237-22.947,7.672-51.254,7.672s-51.255-3.435-51.255-7.672c0-2.174,6.04-4.136,15.747-5.532l.353,0a35.709,35.709,0,0,1,1.875-7.446l-.016,0c-17.836,2.59-29.7,7.2-29.7,12.454,0,8.1,28.207,14.669,63,14.669s63-6.567,63-14.669C214.893,63.352,203.025,58.742,185.19,56.152Z" transform="translate(0 -0.295)" fill="#ffea00"/>
+      <path id="Caminho_2561_-_Contorno" data-name="Caminho 2561 - Contorno" d="M185.19,55.152a1,1,0,0,1,.144.01c8.992,1.306,16.522,3.136,21.776,5.293,5.828,2.392,8.783,5.135,8.783,8.151,0,5.678-10.455,9.3-19.225,11.347C184.7,82.74,168.8,84.275,151.894,84.275s-32.8-1.535-44.775-4.322c-8.771-2.042-19.226-5.668-19.226-11.347,0-3.016,2.955-5.759,8.784-8.151,5.254-2.157,12.784-3.987,21.776-5.293a1,1,0,0,1,.269,0l.016,0a1,1,0,0,1,.813,1.338,34.809,34.809,0,0,0-1.822,7.238,1,1,0,0,1-.985.863l-.284,0a65.244,65.244,0,0,0-11.262,2.42c-3.518,1.21-3.559,2.1-3.559,2.112s.038.868,3.362,2.044a61.525,61.525,0,0,0,10.8,2.392,257.016,257.016,0,0,0,36.095,2.236,257.006,257.006,0,0,0,36.094-2.236,61.524,61.524,0,0,0,10.8-2.392c3.323-1.176,3.361-2.035,3.361-2.044s-.042-.919-3.656-2.145a68.347,68.347,0,0,0-11.578-2.447,1,1,0,0,1-.863-.853,34.806,34.806,0,0,0-1.813-7.183,1,1,0,0,1,.44-1.212l.014-.008A1,1,0,0,1,185.19,55.152Zm-33.3,27.123c16.754,0,32.494-1.517,44.321-4.27A53.722,53.722,0,0,0,209.46,73.44c2.022-1.113,4.433-2.856,4.433-4.834,0-2.067-2.679-4.3-7.543-6.3a91.231,91.231,0,0,0-19.7-4.922,36.724,36.724,0,0,1,1.265,5.283,66.715,66.715,0,0,1,11.216,2.429,14.007,14.007,0,0,1,3.466,1.618,3.038,3.038,0,0,1,1.547,2.422,2.974,2.974,0,0,1-1.451,2.353,12.747,12.747,0,0,1-3.243,1.576,63.388,63.388,0,0,1-11.17,2.485,259.02,259.02,0,0,1-36.39,2.258A259.03,259.03,0,0,1,115.5,75.548a63.391,63.391,0,0,1-11.17-2.485,12.748,12.748,0,0,1-3.243-1.576,2.974,2.974,0,0,1-1.451-2.353,3.017,3.017,0,0,1,1.515-2.4,13.646,13.646,0,0,1,3.393-1.6,66.063,66.063,0,0,1,11.321-2.464,36.72,36.72,0,0,1,1.266-5.284,91.237,91.237,0,0,0-19.7,4.922c-4.864,2-7.543,4.234-7.543,6.3,0,1.978,2.411,3.721,4.433,4.834A53.725,53.725,0,0,0,107.572,78C119.4,80.758,135.139,82.275,151.894,82.275Z" transform="translate(0 -0.295)" fill="#2a2929"/>
+    </g>
+  </g>
+</svg>
+      `,
+    },
+    {
+      nome: "olho",
+      posX: 60,
+      posY: 80,
+      svg: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="118" height="65" viewBox="0 0 118 65">
+      <defs>
+        <clipPath id="clip-olho">
+          <rect width="118" height="65"/>
+        </clipPath>
+      </defs>
+      <g id="olho" clip-path="url(#clip-olho)">
+        <g id="Grupo_340" data-name="Grupo 340" transform="translate(-3134.831 -8790.233)">
+          <ellipse id="Elipse_18" data-name="Elipse 18" cx="59" cy="32.5" rx="59" ry="32.5" transform="translate(3134.831 8790.233)" fill="#fff"/>
+          <path id="Elipse_18_-_Contorno" data-name="Elipse 18 - Contorno" d="M59,2A99.386,99.386,0,0,0,36.488,4.5a71.418,71.418,0,0,0-18.243,6.769,38.091,38.091,0,0,0-12.023,9.8A18.628,18.628,0,0,0,2,32.5,18.628,18.628,0,0,0,6.223,43.932a38.091,38.091,0,0,0,12.023,9.8A71.418,71.418,0,0,0,36.488,60.5,99.386,99.386,0,0,0,59,63a99.386,99.386,0,0,0,22.512-2.5,71.418,71.418,0,0,0,18.243-6.769,38.091,38.091,0,0,0,12.023-9.8A18.628,18.628,0,0,0,116,32.5a18.628,18.628,0,0,0-4.223-11.432,38.091,38.091,0,0,0-12.023-9.8A71.418,71.418,0,0,0,81.512,4.5,99.386,99.386,0,0,0,59,2m0-2c32.585,0,59,14.551,59,32.5S91.585,65,59,65,0,50.449,0,32.5,26.415,0,59,0Z" transform="translate(3134.831 8790.233)" fill="#2a2929"/>
+          <ellipse id="Elipse_19" data-name="Elipse 19" cx="23.9" cy="23.612" rx="23.9" ry="23.612" transform="translate(3169.931 8799.121)" fill="#2a2929"/>
+        </g>
+      </g>
+    </svg>
+    
+        `,
+    },
+  ],
 
-    renderSVG: function (svg, posX, posY) {
-        let local = document.getElementById('backdoor');
+  renderSVG: function (svg, posX, posY) {
+    let local = document.getElementById("backdoor");
 
-        let html = `
+    let html = `
             <div style="
                 position: absolute;
                 top: ${posY}vh;
                 left: ${posX}vw
             ">${svg}</div>
         `;
-        
-        local.innerHTML += html;
 
-        return;
-    },
+    local.innerHTML += html;
 
-    inicia: function (){
-        for (let index = 0; index < Anima.ArraySVG.length; index++) {
-            const element = Anima.ArraySVG[index];
+    return;
+  },
 
-            Anima.renderSVG(element.svg, element.posX, element.posY);
-        }
+  inicia: function () {
+    for (let index = 0; index < Anima.ArraySVG.length; index++) {
+      const element = Anima.ArraySVG[index];
+
+      Anima.renderSVG(element.svg, element.posX, element.posY);
     }
+  },
 };
